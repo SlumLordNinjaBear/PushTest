517d92d4eecf50bc88024d2fea2a93ff396c0bdc
Nicholas Dean <Nicholas Dean>
2016-11-08 16:31:32 -0500

    Initial commit

---
+++ app/src/main/java/unicron/pushtest/MainActivity.java
@@ -0,0 +1,17 @@
+package unicron.pushtest;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+    private void testPush(){
+       int i = 1;
+    }//Test 1
+
+}
